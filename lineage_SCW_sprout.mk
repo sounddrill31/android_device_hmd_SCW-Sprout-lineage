a102175fd11a5751d9c7bb1143ab31567bf2ccdf
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-08-17 17:02:44 +0530

    Update lineage_SCW_sprout.mk

--- lineage_SCW_sprout.mk
+++ lineage_SCW_sprout.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from SCW_sprout device
-$(call inherit-product, device/hmd/SCW_sprout/device.mk)
+# Inherit from SCWsprout device
+$(call inherit-product, device/hmd/SCWsprout/device.mk)
 
-PRODUCT_DEVICE := SCW_sprout
-PRODUCT_NAME := lineage_SCW_sprout
+PRODUCT_DEVICE := SCWsprout
+PRODUCT_NAME := lineage_SCWsprout
 PRODUCT_BRAND := Nokia
 PRODUCT_MODEL := Nokia X10
 PRODUCT_MANUFACTURER := hmd
@@ -25,4 +25,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-hmd
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="ScarletWitch_00WW-user 13 TKQ1.220807.001 00WW_3_390 release-keys"
 
-BUILD_FINGERPRINT := Nokia/ScarletWitch_00WW/SCW_sprout:13/TKQ1.220807.001/00WW_3_390:user/release-keys
+BUILD_FINGERPRINT := Nokia/ScarletWitch_00WW/SCWsprout:13/TKQ1.220807.001/00WW_3_390:user/release-keys
