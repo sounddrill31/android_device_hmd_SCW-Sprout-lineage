7d2f88f14f5554604765f736d70e2ecee532754a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-10-07 10:04:10 +0530

    disable vintf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # VINTF
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+#DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Inherit the proprietary files
 # include vendor/hmd/SCW_sprout/BoardConfigVendor.mk
